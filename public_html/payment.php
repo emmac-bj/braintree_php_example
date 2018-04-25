12f41f9c49b865d99b8186b450990097544be2a7
emmac-bj <38691332+emmac-bj@users.noreply.github.com>
2018-04-25 12:30:49 +0200

    Create payment.php

---
+++ public_html/payment.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once("../includes/braintree_init.php");
+
+
+
+$amount = $_POST["amount"];
+
+$nonce = $_POST["payment_method_nonce"];
+
+
+
+$result = $gateway->transaction()->sale([
+
+    'amount' => $amount,
+
+    'paymentMethodNonce' => $nonce,
+
+    'options' => [
+
+        'submitForSettlement' => true
+
+    ]
+
+]);
+
+
+
+if ($result->success || !is_null($result->transaction)) {
+
+    $transaction = $result->transaction;
+    echo $transaction->id;
+
+}
