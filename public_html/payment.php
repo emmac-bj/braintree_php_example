88006835ebb46303a39de52e160a2507cee8d9c4
emmac-bj <38691332+emmac-bj@users.noreply.github.com>
2018-04-25 13:06:32 +0200

    Update payment.php

--- public_html/payment.php
+++ public_html/payment.php
@@ -6,9 +6,7 @@ require_once("../includes/braintree_init.php");
 
 $amount = $_POST["amount"];
 
-$nonce = $_POST["payment_method_nonce"];
-
-
+$nonce = $_POST["payment_method_nonce";
 
 $result = $gateway->transaction()->sale([
 
@@ -31,4 +29,17 @@ if ($result->success || !is_null($result->transaction)) {
     $transaction = $result->transaction;
     echo $transaction->id;
 
+}
+else {
+
+    $errorString = "";
+
+
+
+    foreach($result->errors->deepAll() as $error) {
+
+        $errorString .= 'Error: ' . $error->code . ": " . $error->message . "\n";
+
+    }
+    echo $errorString;
 }
